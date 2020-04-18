2ad4c9ab218312a0410cc583d18689635c35f241
IPSAE LEE <lareina.ipse@gmail.com>
2020-04-18 13:04:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect
+from scrapper import get_jobs
+
+app = Flask("SuperScrapper")
+
+@app.route("/")
+def home():
+    return render_template("potato.html") 
+
+@app.route("/report")
+def report():
+    word = request.args.get('word')
+    if word:
+        word = word.lower()
+        jobs = get_jobs(word)
+    else:
+        return  redirect("/")
+    return render_template("report.html", searchingBy=word)
+app.run(host="0.0.0.0")
+
