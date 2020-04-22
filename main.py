c320a3b3b4d3204c4bbf66f1f2b012eea40fc0dc
IPSAE LEE <lareina.ipse@gmail.com>
2020-04-22 02:19:36 +0000

    Scrapper Exporter & Download

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from scrapper import get_jobs
+from exporter import save_to_file
 
 app = Flask("SuperScrapper")
 db = {}
@@ -27,5 +28,23 @@ def report():
         resultsNumber=len(jobs),
         jobs = jobs
     )
-app.run(host="0.0.0.0")
+
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get('word')
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        save_to_file(jobs)    
+        return send_file("jobs.csv")
+    except:
+        return redirect("/")
+
+app.run(host="0.0.0.0")   
+
+
 
